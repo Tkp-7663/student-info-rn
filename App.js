241ccc1465165493e212f06fc5a61e0ca65962cb
Tkp-7663 <tharakorn.pangporn2547@gnail.com>
2025-08-19 18:01:21 +0700

    Remove unnecessary React import from App.js

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-// App.js
-import * as React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
